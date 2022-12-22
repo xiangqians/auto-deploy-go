00ea82ed0edda83c20767488542cb9ac88c5c435
xiangqians <shixiangqian.s@gmail.com>
2022-12-22 13:14:30 +0800

    build.bat

--- build.bat
+++ build.bat
@@ -3,4 +3,5 @@ set curDir=%~dp0
 set outputDir=%curDir%build
 set outputPkg="%outputDir%/auto-deploy.exe"
 xcopy i18n "%outputDir%\i18n" /s /e /h /i /y && xcopy static "%outputDir%\static" /s /e /h /i /y && xcopy templates "%outputDir%\templates" /s /e /h /i /y && xcopy data "%outputDir%\data" /s /e /h /i /y && cd ./src && go build -ldflags="-s -w" -o %outputPkg% && upx -9 --brute %outputPkg%
+::xcopy i18n "%outputDir%\i18n" /s /e /h /i /y && xcopy static "%outputDir%\static" /s /e /h /i /y && xcopy templates "%outputDir%\templates" /s /e /h /i /y && xcopy data "%outputDir%\data" /s /e /h /i /y && cd ./src && go build -ldflags="-s -w" -o %outputPkg%
 pause
