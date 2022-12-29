42134ada734dcdca0049513313af39ab6b209f6e
xiangqians <shixiangqian.s@gmail.com>
2022-12-30 00:17:50 +0800

    gox交叉编译

--- build.bat
+++ build.bat
@@ -1,7 +1,22 @@
 ::@echo off
 set curDir=%~dp0
 set outputDir=%curDir%build
-set outputPkg="%outputDir%/o.exe"
-::xcopy i18n "%outputDir%\i18n" /s /e /h /i /y && xcopy static "%outputDir%\static" /s /e /h /i /y && xcopy templates "%outputDir%\templates" /s /e /h /i /y && xcopy data "%outputDir%\data" /s /e /h /i /y && cd ./src && go build -ldflags="-s -w" -o %outputPkg% && upx -9 --brute %outputPkg%
-xcopy i18n "%outputDir%\i18n" /s /e /h /i /y && xcopy static "%outputDir%\static" /s /e /h /i /y && xcopy templates "%outputDir%\templates" /s /e /h /i /y && xcopy data "%outputDir%\data" /s /e /h /i /y && cd ./src && go build -ldflags="-s -w" -o %outputPkg%
+
+:: copy
+xcopy i18n "%outputDir%\i18n" /s /e /h /i /y
+xcopy static "%outputDir%\static" /s /e /h /i /y
+xcopy templates "%outputDir%\templates" /s /e /h /i /y
+xcopy data "%outputDir%\data" /s /e /h /i /y
+
+:: go
+::set outputPkg="%outputDir%/o"
+::cd ./src && go build -ldflags="-s -w" -o %outputPkg%
+::cd ./src && go build -ldflags="-s -w" -o %outputPkg% && upx -9 --brute %outputPkg%
+
+:: gox
+set outputPkg="%outputDir%/o_{{.OS}}_{{.Arch}}"
+::cd ./src && gox -os="windows linux" -output %outputPkg%
+::cd ./src && gox -osarch "windows/amd64 linux/amd64" -output %outputPkg%
+cd ./src && gox -osarch "windows/amd64 linux/amd64" -ldflags="-s -w" -output %outputPkg%
+
 pause
