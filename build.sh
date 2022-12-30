bcd0debcd5a088e24917f27fca65a52a1b002d48
xiangqians <shixiangqian.s@gmail.com>
2022-12-31 03:24:11 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
-CUR_DIR=$(cd $(dirname $0); pwd)
-echo CUR_DIR: ${CUR_DIR}
+curDir=$(cd $(dirname $0); pwd)
+echo curDir: ${curDir}
 
-OUTPUT_DIR=${CUR_DIR}/build
-echo OUTPUT_DIR: ${OUTPUT_DIR}
+outputDir=${curDir}/build
+echo outputDir: ${outputDir}
 
-# build
-OUTPUT_PKG=${OUTPUT_DIR}/o
-echo OUTPUT_PKG: ${OUTPUT_PKG}
-cd ./src && go build -ldflags="-s -w" -o "${OUTPUT_PKG}"
+# 删除 build 目录
+rm -rf "${outputDir}"
+
+# 创建 build 目录
+mkdir -p "${outputDir}"
+
+# cp
+cp -r i18n "${outputDir}/"
+cp -r static "${outputDir}/"
+cp -r templates "${outputDir}/"
+cp -r data "${outputDir}/"
+cp -r script "${outputDir}/"
+
+# go
+os=`go env GOOS`
+arch=`go env GOARCH`
+pkgName=o_${os}_${arch}
+outputPkg=${outputDir}/${pkgName}
+echo outputPkg: ${outputPkg}
+cd ./src && go build -ldflags="-s -w" -o "${outputPkg}"
+
+# startup.sh
+cat>${outputDir}/startup.sh<<EOF
+# startup.sh
+# \$ chmod +x ${pkgName} startup.sh
+./${pkgName}
+EOF
+chmod +x "${outputDir}/startup.sh"
