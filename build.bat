6acfb207c28a8ad94a04307d9077d726a97ad92f
xiangqians <shixiangqian.s@gmail.com>
2022-12-30 21:37:00 +0800

    build.bat

--- build.bat
+++ build.bat
@@ -2,14 +2,18 @@
 set curDir=%~dp0
 set outputDir=%curDir%build
 
+:: rd /?
+:: 删除 build 目录
+rd /s /q %outputDir%
+
 :: copy
-xcopy i18n "%outputDir%\i18n" /s /e /h /i /y
-xcopy static "%outputDir%\static" /s /e /h /i /y
-xcopy templates "%outputDir%\templates" /s /e /h /i /y
-xcopy data "%outputDir%\data" /s /e /h /i /y
-xcopy Dockerfile "%outputDir%" /y
-xcopy docker_build.sh "%outputDir%" /y
-xcopy docker_run.sh "%outputDir%" /y
+xcopy i18n "%outputDir%/i18n" /s /e /h /i /y
+xcopy static "%outputDir%/static" /s /e /h /i /y
+xcopy templates "%outputDir%/templates" /s /e /h /i /y
+xcopy data "%outputDir%/data" /s /e /h /i /y
+::xcopy Dockerfile "%outputDir%" /y
+::xcopy docker_build.sh "%outputDir%" /y
+::xcopy docker_run.sh "%outputDir%" /y
 
 :: go
 ::set outputPkg="%outputDir%/o"
@@ -19,7 +23,17 @@ xcopy docker_run.sh "%outputDir%" /y
 :: gox
 set outputPkg="%outputDir%/o_{{.OS}}_{{.Arch}}"
 ::cd ./src && gox -os="windows linux" -output %outputPkg%
-::cd ./src && gox -osarch "windows/amd64 linux/amd64" -output %outputPkg%
 cd ./src && gox -osarch "windows/amd64 linux/amd64" -ldflags="-s -w" -output %outputPkg%
+
+:: startup.bat
+set startupName=%outputDir%/startup.bat
+echo :: startup.bat > %startupName%
+echo o_windows_amd64.exe >> %startupName%
+echo pause >> %startupName%
+
+:: startup.sh
+set startupName=%outputDir%/startup.sh
+echo # startup.sh > %startupName%
+echo ./o_linux_amd64 >> %startupName%
 
 pause
